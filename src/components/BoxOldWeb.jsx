409cf30767dfcc091c3f653e5ab956627c480b36
TechOfNetwork <168000587+TechOfNetwork@users.noreply.github.com>
2024-06-10 23:04:51 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,7 +2,7 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="https://base.xtkj3phi.repl.co/">
+			<a href="https://msha.ke/tejekate3?fbclid=PAZXh0bgNhZW0CMTEAAabcYw5xIetXYhnzWW3e_GYstsea9L6Qkf2P2ZArFcZzU1J9xoRzqgDlL_A_aem_ATlK-ZF0jO01x_e75NeQeNb6YP1BPKabrLoRDXbVNmYh_DeswVse344H12KzPYxow-TcziZjO2AVk7TOyXPKPS1m">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
